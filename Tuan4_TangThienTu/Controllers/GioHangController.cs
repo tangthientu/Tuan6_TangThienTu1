12a8ff4a3cae101c1eda56d3e208260e92493b24
Administrator <Administrator@DESKTOP-74VMMK9>
2022-03-16 11:07:33 +0700

    Add project files.

---
+++ Tuan4_TangThienTu/Controllers/GioHangController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan4_TangThienTu.Models;
+
+namespace Tuan4_TangThienTu.Controllers
+{
+    public class GioHangController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang == null)
+            {
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.Find(n => n.masach == id);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(id);
+                lstGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+        private int TongSoluong()
+        {
+            int tsl = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                tsl = lstGiohang.Sum(n => n.iSoluong);
+            }
+            return tsl;
+        }
+        private int TongSoluongSanPham()
+        {
+            int tsl = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                tsl = lstGiohang.Count;
+            }
+            return tsl;
+        }
+        private double TongTien()
+        {
+            double tt = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                tt = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return tt;
+        }
+        public ActionResult GioHang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoluong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoluongSanPham();
+            return View(lstGiohang);
+        }
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoluong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoluongSanPham();
+            return PartialView();
+        }
+        public ActionResult XoaGiohang(int id)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.masach == id);
+            if (sanpham != null)
+            {
+                lstGiohang.RemoveAll(n => n.masach == id);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult CapnhatGiohang(int id, FormCollection collection)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.masach == id);
+            if (sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(collection["txtSolg"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            lstGiohang.Clear();
+            return RedirectToAction("GioHang");
+        }
+    }
+}
---
+++ Tuan4_TangThienTu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tuan4_TangThienTu.MvcApplication" Language="C#" %>
